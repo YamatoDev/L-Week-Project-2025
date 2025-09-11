875d15f8c425d6083bb34e9f6ce7782d16c99dbe
YamatoDev <bibaocomatthew@gmail.com>
2025-09-11 21:28:51 +0800

    Revised photography

---
+++ scripts/monster_data.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name MonsterData
+
+@export var material: Material        # Drag your StandardMaterial3D or ShaderMaterial
+@export var is_active: bool = false
--- scripts/screen.gd
+++ scripts/screen.gd
@@ -1,28 +1,42 @@
 extends CSGBox3D
 class_name SpaceScreen
 
-@export var spaceScreen: CSGBox3D
-@export var baseScreenMaterial: Material   # drag a material with your image here
-@export var baseSpaceMaterial: Material   # drag a material with your image here
-@export var monsters: Array[Sprite3D] = []
-@export var monsterStatus: Array[bool] = []
+@export var space_screen: CSGBox3D
+@export var base_screen_material: Material
+@export var normal_screen_material: Material
+@export var monsters: Array[MonsterData] = []
 
-var isActive := false
+var is_active := false
+
 
 func _ready() -> void:
-	spaceScreen.material  = baseScreenMaterial   # start with no material
+	space_screen.material = base_screen_material
 	_update_monsters()
+
 
 func _Activate() -> void:
-	isActive = !isActive
-
-	if isActive:
-		spaceScreen.material  = baseSpaceMaterial
-	else:
-		spaceScreen.material  = baseScreenMaterial  # remove the material
+	is_active = !is_active
+	_update_monster_data(randi_range(0, 5))
 	_update_monsters()
 
+
 func _update_monsters() -> void:
-	var count = min(monsters.size(), monsterStatus.size())
-	for i in count:
-		monsters[i].visible = monsterStatus[i] and isActive
+	if not is_active:
+		space_screen.material = base_screen_material
+		return
+
+	# Find the first monster marked active and set its material
+	for monster in monsters:
+		if monster.is_active:
+			space_screen.material = monster.material
+			return
+
+	# No active monsters
+	space_screen.material = normal_screen_material
+	
+func _update_monster_data(index: int) -> void:
+	if(index > monsters.size()-1):
+		print("Number " + str(index)  + " too big")
+		return
+
+	monsters[index].is_active = !monsters[index].is_active

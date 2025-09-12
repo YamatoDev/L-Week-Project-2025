95275642edd0c597047863d2fc28dfe46033ed15
YamatoDev <bibaocomatthew@gmail.com>
2025-09-12 22:17:00 +0800

    improved on monster data and reworked the updating system
    Update to screen now works whether one sends an index value or string
    
    (Ex. Getting Golshi to appear on screen can be done by calling _update_monster_data(2) or _update_monster_data("Golshi"))

--- scripts/camera_3d.gd
+++ scripts/camera_3d.gd
@@ -2,25 +2,35 @@ extends Camera3D
 
 var mouse = Vector2()
 var flipped := false
+var last_toggle_time := 0
+var toggle_cooldown := 1000   # milliseconds (0.3s)
 
 func _input(event):
 	if event is InputEventMouse:
 		mouse = event.position
+
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			get_selection()
-	
+
 	if Input.is_action_pressed("change_cam"):
-		_toggle_camera_rotation() 
+		_try_toggle_camera_rotation()
+
 
 func get_selection():
 	var worldspace = get_world_3d().direct_space_state
 	var start = project_ray_origin(mouse)
 	var end = start + project_ray_normal(mouse) * 1000
 	var result = worldspace.intersect_ray(PhysicsRayQueryParameters3D.create(start, end))
-	print(result)
 	if result and result.collider.has_method("on_interact"):
 		result.collider.on_interact()
+
+
+func _try_toggle_camera_rotation():
+	var now = Time.get_ticks_msec()
+	if now - last_toggle_time >= toggle_cooldown:
+		_toggle_camera_rotation()
+		last_toggle_time = now
 
 
 func _toggle_camera_rotation() -> void:

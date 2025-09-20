dece17d4695cad79c65dfaaaf93ad86f4480da2e
YamatoDev <bibaocomatthew@gmail.com>
2025-09-20 20:08:46 +0800

    collision

--- scenes/monster.gd
+++ scenes/monster.gd
@@ -1,15 +1,18 @@
 extends Area3D
 
+@export var monster: TextureData
 var ship_inside := false
 
 func _on_area_entered(body):
-	print(body.name)
-	if body.name == "ship":
+	print("Entered: " + body.name)
+	if body.name == "MonsterCheck":
 		print("Entered")
+		monster.is_active = true
 		ship_inside = true
 
 func _on_area_exited(body):
-	print(body.name)
-	if body.name == "ship":
+	print("Exited: " + body.name)
+	if body.name == "MonsterCheck":
 		print("Exited")
+		monster.is_active = false
 		ship_inside = false
